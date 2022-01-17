a89077f36396fe5658f6405795bc45af81475973
gktin <gktin@yandex.ru>
2022-01-17 12:35:45 +0700

    remove webpack configs

--- webpack/dev.js
+++ webpack/dev.js
@@ -1,10 +1,33 @@
-const common = require('./common')
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    ...common,
-
     mode: 'development',
     devtool: 'eval-source-map',
+    entry: './src/index.ts',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        })
+    ],
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
 
     devServer: {
         historyApiFallback: true,
