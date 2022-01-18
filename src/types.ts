f54a84c916524effdfc5049d2322102667faf526
gktin <gktin@yandex.ru>
2022-01-18 23:54:39 +0700

    remove unused types

--- src/types.ts
+++ src/types.ts
@@ -5,11 +5,3 @@ export type Leaf<T> = T;
 export type MergeResult<F, S> = S | S & F;
 
 export type PathObject<T> = { [key: string]: PathObject<T> | Leaf<T> };
-
-// export type CreatePathObject<T> = (leafValue: T, path: Path) => PathObject<T>;
-//
-// export type NaiveClone<T> = (value: T) => T;
-//
-// export type DeepMerge<T, Q> = (first: T, second: Q) => MergeResult<T, Q>;
-//
-// export type GetPathTo<T> = (leafValue: T, nested: PathObject<T> | T) => Path;
